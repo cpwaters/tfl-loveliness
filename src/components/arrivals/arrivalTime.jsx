c791a5eab0072a4e17276a01ca4c317388b256a2
Chris Waters <chris.waters@havas.com>
2022-03-23 19:17:57 +0000

    create display board

--- src/components/arrivals/arrivalTime.jsx
+++ src/components/arrivals/arrivalTime.jsx
@@ -5,7 +5,9 @@ const ArrivalTime = ({ stopCode }) => {
 
   const { data, loading } = useFetch(`https://api.tfl.gov.uk/stoppoint/${stopCode}/arrivals`);
 
-//console.log(data)
+  const [isClock, setIsClock] = useState()
+
+ console.log(data)
 
   const renderName = (x) => {
     if(x * 60 === 0) {return("Arrived")}
@@ -15,28 +17,64 @@ const ArrivalTime = ({ stopCode }) => {
     
   }
 
-  const tts = data && data.map(time => (time))
+  //const tts = data && data.map(time => (time))
   //console.log(tts)
-  const orderedList = tts && tts.sort((a,b) => a.timeToStation > b.timeToStation ? 1 : -1);
+  //const orderedList = tts && tts.sort((a,b) => a.timeToStation > b.timeToStation ? 1 : -1);
 
-function getTimeClock() {
-  const currentTime = document.getElementById('time');
+
+  useEffect(() => {
+    setInterval(() => {
+      const time = new Date();
+      let h = time.getHours();
+      let m = time.getMinutes();
+      let s = time.getSeconds();
+      h = h <= 9 ? "0"+h : h ;
+      m = m <= 9 ? "0"+m : m ;
+      s = s <= 9 ? "0"+s : s ;
+      let theClock = `${h}:${m}:${s}`;
+      setIsClock(theClock);
+  },10);
+  },[])
   
-  const time = new Date();
-  let h = time.getHours();
-  let m = time.getMinutes();
-  let s = time.getSeconds();
-  h = h <= 9 ? "0"+h : h ;
-  s = s <= 9 ? "0"+s : s ;
-  currentTime.innerHTML = h+":"+m+":"+s;
-  }
+  const arrivals = [
+    {
+      order: '1st',
+      time: '15:00',
+      destination: 'Hadfield',
+      //destination: 'data && data[0].towards',
+      expected: 'On time'
+    },
+    {
+      order: '2st',
+      time: '17:00',
+      destination: 'Hadfield',
+      expected: '17:11'
+    },
+  ];
   
-  setInterval(getTimeClock,10);
+  const arrivalObj = arrivals.map(obj => Object.entries(obj).map(([key,val]) => <div className={key}>{val}</div>));
+
+  const destinations = [
+    'Ashburys',
+    'Gorton',
+    "Guide Bridge",
+    'Flowery Field',
+    'Newton for Hyde',
+    'Godley',
+    'Hattersley',
+    'Broadbottom',
+    'Dinting',
+    'Glossop',
+    'Hadfield'
+  ];
+  
+  let cad = '';
+  destinations.map((d,i) => i === 0 ? cad += d : cad += ", " + d);
 
   return (
       <>
       {/*}
-      {loading ? <div className="loader"></div> : 
+      {loading ? <div classNameName="loader"></div> : 
         data && data.map(at => (
           <ul>
             <li>bearing: {at.bearing}</li>
@@ -64,17 +102,23 @@ function getTimeClock() {
         )
       } */}
 
-      <div className="board-frame">
-        <div className="board-frame-inner">
-          <div className="board-screen-outer">
-            <div className="callingAtStrip">
-              <span className="callingAt">Calling at: </span>
-              <span id="cad" className="callingAtDestinations"></span>
+        <div className="board-frame">
+          <div className="board-frame-inner">
+            <div className="board-screen-outer">
+              <div className="arrival">
+              <div className="singleArrival">{arrivalObj[0]}</div>
+                <div className="callingAtStrip"> 
+                    <div className="callingAt">Calling at: </div>
+                    <div className="callingAtDestinations">{cad}</div>
+                </div>
+                <div className="singleArrival">{arrivalObj[1]}</div>
+              </div>
+              
+            
+              <div className="clock">{isClock}</div>
             </div>
-            <div id="time" className="time"></div>
           </div>
         </div>
-      </div>
      </>
   )
 }
